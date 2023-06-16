9e5a315bc29ba32f30c9e5a1c5f73e0faa9f5a2e
furkan837 <80164634+imFrKn@users.noreply.github.com>
2023-06-16 16:56:29 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -66,12 +66,15 @@ class BigCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-     final theme = Theme.of(context);      
+     final theme = Theme.of(context);  
+     final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
     return Card(
       color: theme.colorScheme.primary,  
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(pair.asLowerCase),
+      padding: const EdgeInsets.all(20),
+       child: Text(pair.asLowerCase, style: style),
       ),
     );
   }
